e3098da12b0bcb19fef37bbc3139a9520cf10372
jordenskraften <97627971+jordenskraften@users.noreply.github.com>
2023-05-12 00:10:04 +0600

    database update

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from products.models import ProductCategory, Product
 
 def index(request): 
     context = {
@@ -10,32 +11,8 @@ def index(request):
 def products(request):
     context = {
         'title' : 'Store - Каталог',
-        'products': [
-            {
-                'image': 'vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': '6 090,00 руб.',
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-            },
-            {
-                'image': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': '23 725,00 руб.',
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-            },
-            {
-                'image': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': '3 390,00 руб.',
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-            },
-            {
-                'image': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-                'name': 'Черный рюкзак Nike Heritage',
-                'price': '2 340,00 руб.',
-                'description': 'Плотная ткань. Легкий материал.',
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     } 
     return render(request, 'products.html', context)
 
--- store/store/urls.py
+++ store/store/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 from products.views import index, products
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', index, name='index'), 
     path('products/', products, name='products'), 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
